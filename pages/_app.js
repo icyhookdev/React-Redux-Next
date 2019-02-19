9b45d07ab5033fbe0a1024942b5fe23840c62b8c
thienJs <cristian.albarran95@gmail.com>
2019-02-19 12:35:26 -0400

    feat(file name): root page
    defining root page explicity
    
    BREAKING CHANGE: redux won't work if the file is created

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App, { Container } from 'next/app';
+import withRedux from 'next-redux-wrapper';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducers from '../store/reducer';
+
+const makeStore = () => {
+  return createStore(rootReducers, composeWithDevTools());
+};
+
+class App extends App {
+  render() {
+    const { Component, pageProps, store } = this.props;
+    return (
+      <Container>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
+  }
+}
+
+export default withRedux(makeStore)(App);
