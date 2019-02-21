f1236b8eff0f58975700c849f73f12a68f223292
thienJs <cristian.albarran95@gmail.com>
2019-02-21 15:00:39 -0400

    fix(next config): css moudles
    code comment in disallow node sass

--- next.config.js
+++ next.config.js
@@ -2,3 +2,15 @@ const withSass = require('@zeit/next-sass');
 module.exports = withSass({
   cssModules: true
 });
+
+// const withSass = require('@zeit/next-sass');
+// const withCss = require('@zeit/next-css');
+
+// module.exports = withSass(
+//   withCss({
+//     webpack: config => {
+//       config.module.rules.push({ test: /\.scss$/, loader: ['css-loader'] });
+//       return config;
+//     }
+//   })
+// );
