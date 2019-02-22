c85aef3c53f238a8c4acae5e58026ad01bb6ff04
thienJs <cristian.albarran95@gmail.com>
2019-02-22 17:11:39 -0400

    feat(styled): ui
    header & card ui changed

--- css/components/index.js
+++ css/components/index.js
@@ -14,22 +14,33 @@ export const Paragraph = styled.p`
   color: ${props => props.TextColor || 'black'};
 `;
 
-export const Card = styled.li`
-  flex-basic: 49%;
+export const Card = styled.div`
+  width: 300px;
   padding: 1rem;
   background: #f2f2f2;
   border-radius: 5px;
   margin-bottom: 1rem;
+  height: 100%;
+  text-align: center;
 `;
 
 export const Wrapper = styled.ul`
   margin-top: 5rem;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: space-around;
   padding: 0;
 `;
 
-// export const Header = styled.header`
-
-// `;
+export const BigTittle = styled.h1`
+  font-size: 4rem;
+  font-weight: bold;
+  margin-top: 30px;
+`;
+export const HeaderImg = styled.header`
+  width: 100%;
+  height: 400px;
+  padding: 30px
+  background: linear-gradient(19deg, #21d4fd63 0%, #b721ff63 100%),
+    url('../../img/posts.jpeg');
+`;
